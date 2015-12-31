5bddca5ad6ebcbf22dac63e5dd47281717fcc77a
TristanRhodes <TristanJERhodes@yahoo.co.uk>
2015-12-31 10:37:22 +0000

    Added basic app to repolli.

---
+++ src/BBK.App/Controllers/BranchesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class BranchesController : Controller
+    {
+        // GET: api/core/branches
+        [HttpGet("branches")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Balham", "Elephant and Castle" };
+        }
+    }
+}
---
+++ src/BBK.App/Models/Basket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class Basket
+    {
+        public Basket(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
